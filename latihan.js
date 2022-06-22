b5ed93558ee3ef0bf6ad3aaccaacdeccff922bac
noffrizal <zaimjr@gmail.com>
2022-06-22 12:03:09 +0700

    update latihan.js

--- latihan.js
+++ latihan.js
@@ -1,1 +1,21 @@
-alert('hello noff');
+// x = 18;
+// alert('nilai dari x : adalah ' + x);
+// var nama = prompt('masukkan nama : ');
+// alert('Nama Saya adalah '+ nama);
+// var konfirmasi = confirm('kamu yakin?');
+// if (konfirmasi === true) {
+//     alert('user menekan tombol OK');
+// } else {
+//     alert('user menekan tombol Cancel');
+// }
+
+alert('selamat datang');
+var lagi = true;
+while( lagi){
+    var nama = prompt('masukkan nama');
+    alert('halo '+ nama);
+
+    lagi= confirm('coba lagi?');
+}
+
+alert('terima kasih');
